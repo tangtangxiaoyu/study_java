3a99f573d0ceb5224315e80e9f5e1802d752c23d
tangxiaoyu <15932274470@163.com>
2023-09-15 11:32:53 +0800

    1

---
+++ chapter12/src/Exception04.java
@@ -0,0 +1,26 @@
+public class Exception04 {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person();
+            person = null;
+            System.out.println(person.getName());
+            int n1 = 10;
+            int n2 = 0;
+            int res = n1 / n2;
+        } catch (NullPointerException e) {
+            System.out.println("空指针异常" + e.getMessage());
+        } catch (ArithmeticException e){
+            System.out.println("算数异常" +e.getMessage());
+        } finally {
+
+        }
+    }
+}
+
+class Person {
+    private String name = "jack";
+
+    public String getName() {
+        return name;
+    }
+}
