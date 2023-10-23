2347aa0fea7a424edeb6d717b826abee531c65d7
15932274470 <15932274470>
2023-10-23 22:59:24 +0800

    no message

---
+++ src/chapter16/draw/draw01.java
@@ -0,0 +1,17 @@
+package chapter16.draw;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class draw01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class MyPanel extends JPanel{
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
