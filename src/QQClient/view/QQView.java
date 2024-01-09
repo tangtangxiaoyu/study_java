b2c11b7d5a54861602ea5fc8074efe623b36e335
tangxiaoyu <15932274470@163.com>
2024-01-09 16:55:48 +0800

    1

--- src/QQClient/view/QQView.java
+++ src/QQClient/view/QQView.java
@@ -8,6 +8,7 @@ public class QQView {
 
     public static void main(String[] args) {
         new QQView().mainView();
+        System.out.println("客户端退出系统");
     }
 
     private void mainView(){
@@ -15,11 +16,47 @@ public class QQView {
             System.out.println("==================欢迎登录=====================");
             System.out.println("\t\t1.登录系统");
             System.out.println("\t\t9.退出系统");
+            System.out.println("请输入您的选择");
 
             key = Utility.readString(1);
             switch (key){
                 case "1":
-                    System.out.println("登录系统");
+                    System.out.println("请输入用户名:");
+                    String userId = Utility.readString(50);
+                    System.out.println("请输入秘密:");
+                    String pwd = Utility.readString(50);
+                    if(true){
+                        System.out.println("==================欢迎"+ userId +"=====================");
+                        while (loop){
+                            System.out.println("\n==============二级菜单"+ userId +"=====================");
+                            System.out.println("\t\t1.显示在线用户列表");
+                            System.out.println("\t\t2.群发消息");
+                            System.out.println("\t\t3.私聊消息");
+                            System.out.println("\t\t4.发送文件");
+                            System.out.println("\t\t9.退出系统");
+                            System.out.print("请输入您的选择");
+                            key = Utility.readString(1);
+                            switch (key){
+                                case "1":
+                                    System.out.println("显示在线用户列表");
+                                    break;
+                                case "2":
+                                    System.out.println("群发消息");
+                                    break;
+                                case "3":
+                                    System.out.println("私聊消息");
+                                    break;
+                                case "4":
+                                    System.out.println("发送文件");
+                                    break;
+                                case "9":
+                                    loop = false;
+                                    break;
+                            }
+                        }
+                    }else{
+                        System.out.println("登录失败");
+                    }
                     break;
                 case "9":
                     loop = false;
