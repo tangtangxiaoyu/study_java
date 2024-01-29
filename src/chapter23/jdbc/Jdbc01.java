b13229fa25471b77ddb6bc2999dd385d02943cab
tangxiaoyu <15932274470@163.com>
2024-01-29 15:03:22 +0800

    1

--- src/chapter23/jdbc/Jdbc01.java
+++ src/chapter23/jdbc/Jdbc01.java
@@ -1,4 +1,4 @@
-package com.hspedu.jdbc;
+package chapter23.jdbc;
 
 import com.mysql.jdbc.Driver;
 
@@ -7,11 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-/**
- * @author 韩顺平
- * @version 1.0
- * 这是第一个Jdbc 程序，完成简单的操作
- */
+
 public class Jdbc01 {
     public static void main(String[] args) throws SQLException {
 
@@ -32,13 +28,13 @@ public class Jdbc01 {
         Properties properties = new Properties();
         //说明 user 和 password 是规定好，后面的值根据实际情况写
         properties.setProperty("user", "root");// 用户
-        properties.setProperty("password", "......"); //密码
+        properties.setProperty("password", "....."); //密码
         Connection connect = driver.connect(url, properties);
 
         //3. 执行sql
-        //String sql = "insert into actor values(null, '刘德华', '男', '1970-11-11', '110')";
+        String sql = "insert into dept values(10,'12','sda')";
         //String sql = "update actor set name='周星驰' where id = 1";
-        String sql = "delete from actor where id = 1";
+        //String sql = "delete from actor where id = 1";
         //statement 用于执行静态SQL语句并返回其生成的结果的对象
         Statement statement = connect.createStatement();
         int rows = statement.executeUpdate(sql); // 如果是 dml语句，返回的就是影响行数
@@ -51,3 +47,4 @@ public class Jdbc01 {
 
     }
 }
+
